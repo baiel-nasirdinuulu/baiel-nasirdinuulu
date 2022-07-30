d939a403f3f03b011c4fd7d906f9a5b17a930bb3
baiel nasirdinuulu <baiel.nasirdin.alt@gmail.com>
2022-07-31 00:50:22 +0600

    add new things

---
+++ style.js
@@ -0,0 +1,15 @@
+let newb = (document.getElementById("newb").style.display = "none");
+
+function press() {
+  let header = document.getElementById("header");
+  header.style.display = "flex";
+  let button = (document.getElementById("but").style.display = "none");
+  let butto = (document.getElementById("newb").style.display = "flex");
+}
+
+function newbut() {
+  let header = document.getElementById("header");
+  header.style.display = "none";
+  let button = (document.getElementById("newb").style.display = "none");
+  let butto = (document.getElementById("but").style.display = "flex");
+}
